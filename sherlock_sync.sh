2a78b78625d116fda2637c80e5fc747aa6321996
Shubh Gupta <shubhg1996@gmail.com>
2020-02-22 20:33:53 -0800

    fixed sync

--- sherlock_sync.sh
+++ sherlock_sync.sh
@@ -2,13 +2,13 @@
 
 opt=$1
 path=`readlink -f $2`
-relpath=${path#/mnt/c/Users/mg220/Documents/}
+relpath=${path#/mnt/c/Users/shubh/Documents/}
 if [ $opt == "send" ]
 then
-    scp -r $path sherlock:~/${relpath}
+    scp -r $path shubhgup@sherlock.stanford.edu:~/${relpath}
 elif [ $opt == "get" ]
 then
-    scp -r sherlock:~/${relpath} $path
+    scp -r shubhgup@sherlock.stanford.edu:~/${relpath} $path
 else
     echo "Wrong OP"
 fi
